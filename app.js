8e127f3a1e7d983395b85f66828bd684e571e207
hemanbhullar2 <163816819+hemanbhullar2@users.noreply.github.com>
2024-03-18 11:39:09 +0530

    api with post method

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 
 const port = 3000;
 
-app.post('testbfhl.herojuapp.com/bfhl', (req, res) => {
-    
+const mongoose = require('mongoose');
+
+const user = mongoose.schema(
+    'user_id':{type:String, required:true},
+    'roll_number':{type:String, required:true},
+    'odd_numbers':{type:Number, isOdd:true},
+    'even_number':{type:Number, isEven:true},
+    'alphabets':{type:String, isAlphabet: true},
+);
+
+app.post('/testbfhl.herojuapp.com/bfhl', (req, res) => {
+    res.send(user);
 })
+
+app.listen(port);
+
+console.log("server started at http://testbfhl.herojuapp.com/bfhl");
